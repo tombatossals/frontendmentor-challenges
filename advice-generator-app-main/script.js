72a4777b9f2e07f45e5e0ae3b2640ca9b6beba64
David Rubert <david.rubert@gmail.com>
2022-06-11 09:57:43 +0200

    update

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp(
+    {
+        template: `
+<div id="app" class="advice" v-if="slip">
+    <h1 class="advice__title">Advice #{{ slip.id }}</h1>
+    <div class="advice__content">"{{slip.advice}}"</div>
+    <div class="advice__quote">
+        <div class="quote__line"></div>
+        <div class="quote__quote quote1"></div>
+        <div class="quote__quote quote2"></div>
+        <div class="quote__line"></div>
+    </div>
+    <div class="advice__dice" v-on:click="fetchAdvice">
+        <img class=" advice__image__dice" alt="dice" src="images/icon-dice.svg" />
+    </div>
+</div>`,
+    data() {
+        return {
+          slip: {}
+        }
+      },
+      methods: {
+        async fetchAdvice() {
+          const req = await fetch("https://api.adviceslip.com/advice?id=" + Date.now());
+          const {
+            slip
+          } = await req.json();
+          this.slip = slip;
+        }
+      },
+      mounted() {
+        this.fetchAdvice()
+      }
+    }  
+)
+
+app.mount("#app")
